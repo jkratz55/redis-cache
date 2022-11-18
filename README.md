32b0f99a60cc29e9cc2d6379cdb5e3daa7a062b1
Joseph Kratz <4985721+jkratz55@users.noreply.github.com>
2022-11-18 13:02:52 -0500

    Added unit tests and README

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# redis-cache
-A simple cache library that is backed by Redis
+# Redis Cache
+
+Redis Cache is a very simple abstraction over Redis for basic caching functionality. It can be thought of as a simple Cache backed by Redis. Redis Cache provides the following functionality: Set, Get, and Delete. In addition, there is a SetWithTTL method to set entries that will automatically be removed once the TTL has expired.
+
+## Requirements
+
+* Go 1.18+ (might work on older versions of Go but untested)
+* Redis 7
+* go-redis/redis (this is the backing Redis library/client)
+
+## Getting Redis Cache
+
+```shell
+go get github.com/jkratz/redis-cache
+```
+
+## Usage
+
+Under the hood Redis Cache was designed to be used with [https://github.com/go-redis/redis](https://github.com/go-redis/redis). However, it can work with anytime that implements the `redisClient` interface.
+
+```go
+type redisClient interface {
+	Get(ctx context.Context, key string) *redis.StringCmd
+	Set(ctx context.Context, key string, val any, ttl time.Duration) *redis.StatusCmd
+	Del(ctx context.Context, keys ...string) *redis.IntCmd
+}
+```
+
+This means that `Cache` type can work with the following types.
+
+* redis.Client
+* redis.ClusterClient
+* redis.Ring
+
+You'll need to choose the right type based on your particular use cases. More information can be found [here](https://github.com/go-redis/redis)
+
+For the example below we are going to assume a single standalone Redis node and use Client. Out of the box Cache uses msgpack to marshall and unmarshall the value. However, if you want to use a different serialization method you can provide it as an option when creating a Cache instance using NewCache.
+
+```go
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-redis/redis/v9"
+
+	rcache "github.com/jkratz55/redis-cache"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	cache := rcache.NewCache(client)
+
+	if err := cache.Set(context.Background(), "person", Person{
+		FirstName: "Biily",
+		LastName:  "Bob",
+		Age:       45,
+	}); err != nil {
+		panic("ohhhhh snap!")
+	}
+
+	var p Person
+	if err := cache.Get(context.Background(), "person", &p); err != nil {
+		panic("ohhhhh snap")
+	}
+	fmt.Printf("%v\n", p)
+
+	if err := cache.Delete(context.Background(), "person"); err != nil {
+		panic("ohhh snap!")
+	}
+
+	if err := cache.Get(context.Background(), "person", &p); err != rcache.ErrKeyNotFound {
+		panic("ohhhhh snap, this key should be gone!")
+	}
+}
+```
--- cache_test.go
+++ cache_test.go
@@ -1,1 +1,170 @@
 package cache
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis"
+	"github.com/go-redis/redis/v9"
+	"github.com/stretchr/testify/assert"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+var server *miniredis.Miniredis
+var client *redis.Client
+
+func setup() {
+	server = mockRedis()
+	client = redis.NewClient(&redis.Options{
+		Addr: server.Addr(),
+	})
+}
+
+func tearDown() {
+	server.Close()
+	client.Close()
+}
+
+func mockRedis() *miniredis.Miniredis {
+	s, err := miniredis.Run()
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
+
+func TestNewCache(t *testing.T) {
+	assert.NotPanics(t, func() {
+		NewCache(client)
+	})
+}
+
+func TestCache_Get(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	val, _ := msgpack.Marshal("value123")
+	if err := client.Set(context.Background(), "key123", val, 0).Err(); err != nil {
+		t.Errorf("failed to setup data in Redis")
+	}
+	val, _ = msgpack.Marshal("value456")
+	if err := client.Set(context.Background(), "key456", val, 0).Err(); err != nil {
+		t.Errorf("failed to setup data in Redis")
+	}
+
+	cache := NewCache(client)
+
+	var s string
+	err := cache.Get(context.Background(), "key123", &s)
+	assert.NoError(t, err)
+	assert.Equal(t, "value123", s)
+
+	err = cache.Get(context.Background(), "key456", &s)
+	assert.NoError(t, err)
+	assert.Equal(t, "value456", s)
+
+	err = cache.Get(context.Background(), "random", &s)
+	assert.ErrorIs(t, err, ErrKeyNotFound)
+}
+
+func TestCache_Set(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	cache := NewCache(client)
+
+	err := cache.Set(context.Background(), "key123", "value123")
+	assert.NoError(t, err)
+
+	err = cache.Set(context.Background(), "key456", "value456")
+	assert.NoError(t, err)
+
+	b, err := client.Get(context.Background(), "key123").Bytes()
+	assert.NoError(t, err)
+
+	var s string
+	err = msgpack.Unmarshal(b, &s)
+	assert.NoError(t, err)
+	assert.Equal(t, "value123", s)
+}
+
+func TestCache_SetWithTTL(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	cache := NewCache(client)
+
+	err := cache.SetWithTTL(context.Background(), "key123", "value123", time.Second*1)
+	assert.NoError(t, err)
+
+	count, err := client.Exists(context.Background(), "key123").Result()
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), count)
+
+	dur, err := client.TTL(context.Background(), "key123").Result()
+	assert.NoError(t, err)
+	assert.Equal(t, time.Second*1, dur)
+}
+
+func TestCache_Delete(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	client.Set(context.Background(), "key123", "value123", 0)
+	client.Set(context.Background(), "key456", "value456", 0)
+
+	cache := NewCache(client)
+	err := cache.Delete(context.Background(), "key123", "key456")
+	assert.NoError(t, err)
+
+	err = cache.Get(context.Background(), "key123", nil)
+	assert.ErrorIs(t, err, ErrKeyNotFound)
+
+	err = cache.Get(context.Background(), "key456", nil)
+	assert.ErrorIs(t, err, ErrKeyNotFound)
+}
+
+func TestNewCache_CustomSerialization(t *testing.T) {
+	setup()
+	defer tearDown()
+
+	marshaller := Marshaller(func(v any) ([]byte, error) {
+		var buffer bytes.Buffer
+		err := gob.NewEncoder(&buffer).Encode(v)
+		return buffer.Bytes(), err
+	})
+
+	unmarshaller := Unmarshaller(func(b []byte, v any) error {
+		reader := bytes.NewReader(b)
+		return gob.NewDecoder(reader).Decode(v)
+	})
+
+	type Person struct {
+		FirstName  string
+		MiddleName string
+		LastName   string
+		Age        int
+	}
+
+	cache := NewCache(client, Serialization(marshaller, unmarshaller))
+	err := cache.Set(context.Background(), "person1", Person{
+		FirstName:  "Billy",
+		MiddleName: "Joel",
+		LastName:   "Bob",
+		Age:        99,
+	})
+	assert.NoError(t, err)
+
+	var p Person
+	err = cache.Get(context.Background(), "person1", &p)
+	assert.NoError(t, err)
+	assert.Equal(t, Person{
+		FirstName:  "Billy",
+		MiddleName: "Joel",
+		LastName:   "Bob",
+		Age:        99,
+	}, p)
+}
